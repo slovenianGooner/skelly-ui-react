6a3589adf4ec9aae04200efa14a8498b63f8d8de
Lenart Papež <lenartpapez9@gmail.com>
2023-05-25 11:06:21 +0200

    tsconfig + inputs adjustments for typescript

--- src/lib/components/RadioInput.tsx
+++ src/lib/components/RadioInput.tsx
@@ -1,18 +1,16 @@
-type RadioInputProps = {
-    label: string,
-    checked: boolean,
-    value: string,
-    onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+import { InputHTMLAttributes } from "react"
+
+interface RadioInputProps extends InputHTMLAttributes<HTMLInputElement> {
+    label: string
 }
 
-export default function RadioInput({label, checked, value, onChange}: RadioInputProps) {
+export default function RadioInput({ label, ...props }: RadioInputProps) {
     return (
         <label className="flex items-center">
             {/* Some special checked: classes due to a dark mode tailwindcss bug for radio inputs */}
-            <input type="radio" className="h-4 w-4 bg-white dark:bg-white/5 border-gray-300 dark:border-white/10 text-indigo-600 dark:checked:bg-indigo-500 dark:checked:border-indigo-500 focus:ring-indigo-600 dark:focus:ring-indigo-500 dark:focus:ring-offset-gray-900"
-                   checked={checked}
-                   value={value}
-                   onChange={onChange}
+            <input
+                {...props}
+                type="radio" className="h-4 w-4 bg-white dark:bg-white/5 border-gray-300 dark:border-white/10 text-indigo-600 dark:checked:bg-indigo-500 dark:checked:border-indigo-500 focus:ring-indigo-600 dark:focus:ring-indigo-500 dark:focus:ring-offset-gray-900"
             />
             <span className="ml-2 text-sm text-gray-900 dark:text-white">{label}</span>
         </label>
--- src/lib/components/ToggleInput.tsx
+++ src/lib/components/ToggleInput.tsx
@@ -7,19 +7,18 @@ type ToggleInputProps = {
 }
 
 export default function ToggleInput({checked, onChange, label}: ToggleInputProps) {
-    let enabled = checked;
 
     const classNames = function (...classes) {
         return classes.filter(Boolean).join(' ')
     }
 
     return (
-        <div className="flex items-center">
+        <Switch.Group as="div" className="flex items-center">
             <Switch
-                checked={enabled}
+                checked={checked}
                 onChange={onChange}
                 className={classNames(
-                    enabled ? 'bg-indigo-600 dark:bg-indigo-500' : 'bg-gray-200 dark:bg-white/10',
+                    checked ? 'bg-indigo-600 dark:bg-indigo-500' : 'bg-gray-200 dark:bg-white/10',
                     'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-indigo-600 dark:focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-900'
                 )}
             >
@@ -27,12 +26,14 @@ export default function ToggleInput({checked, onChange, label}: ToggleInputProps
                 <span
                     aria-hidden="true"
                     className={classNames(
-                        enabled ? 'translate-x-5' : 'translate-x-0',
+                        checked ? 'translate-x-5' : 'translate-x-0',
                         'pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out'
                     )}
                 />
             </Switch>
-            <span className="ml-2 text-sm text-gray-900 dark:text-white">{label}</span>
-        </div>
+            <Switch.Label as="span" className="ml-2 text-sm text-gray-900 dark:text-white">
+                {label}
+            </Switch.Label>
+        </Switch.Group>
     )
 }
